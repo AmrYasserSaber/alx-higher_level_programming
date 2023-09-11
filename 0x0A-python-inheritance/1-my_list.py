de3d5e5697f4b471699a05ce41feab995debb24b
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-11 15:48:21 +0300

    Fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """python file"""
 
-class myList(list):
+
+class MyList(list):
     """class that inherits from list"""
+
     def print_sorted(self):
         """print a sorted list"""
         print(sorted(self))
