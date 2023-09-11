de65a462e3166c1eb2959a72846d124fe78d8f7a
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-11 15:34:39 +0300

    fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,7 @@
+#!/usr/bin/python3
+"""python file"""
+
+
 def lookup(obj):
-    """
-    Return a list of available attributes and methods of an object.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list of strings containing the names of attributes and methods.
-    """
-    import types  # Import the types module here
-
-    if hasattr(obj, '__dir__'):
-        return [attr for attr in dir(obj)]
-    else:
-        return []
+    """return the attributes and methode of an object"""
+    return dir(obj)
