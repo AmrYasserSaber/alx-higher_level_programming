a5fe25ab1715a43bd5d2e9532e2edb4a6a81a4e9
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-12 20:19:02 +0300

    Fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,5 +4,15 @@ import json
 
 
 def class_to_json(obj):
-    """Return the dictionary description with simple data structure."""
-    return obj.__dict__
+    # Get the class attributes and their values
+    attributes = obj.__dict__
+
+    # Initialize a dictionary to store the JSON-serializable representation
+    json_dict = {}
+
+    for key, value in attributes.items():
+        # Check if the value is a serializable type
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+
+    return json_dict
