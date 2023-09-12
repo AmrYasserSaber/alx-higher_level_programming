05495bbf46ea8cc22681bc1e41c4af89112595e5
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-12 20:22:29 +0300

    Fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-"""Return the dictionary description with simple data structure."""
-import json
 
 
 def class_to_json(obj):
-    if isinstance(obj, (str, int, bool)):
-        return obj
-
-    if isinstance(obj, list):
-        return [class_to_json(item) for item in obj]
-
-    if isinstance(obj, dict):
-        return {key: class_to_json(value) for key, value in obj.items()}
-
-    if hasattr(obj, '__dict__'):
-        data = {}
-        for key, value in obj.__dict__.items():
-            data[key] = class_to_json(value)
-        return data
+    return vars(obj)
