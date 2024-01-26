abcceaa31cdf473439c1b6e08e584e46eef15c91
Amr Yasser Saber <96260095+AmrYasserSaber@users.noreply.github.com>
2024-01-27 00:00:31 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-const number = Math.floor(parseInt(arg));
-if (isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${number}`);
-}
