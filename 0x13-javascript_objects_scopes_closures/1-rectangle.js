024aa12947aaf51a776fb80fe53b5afbdde16d8e
Amr Yasser Saber <amr.yassr.2@gmail.com>
2024-02-14 18:40:28 +0200

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// empty class named rectangle with constructor
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
